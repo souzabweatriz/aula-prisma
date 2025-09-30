7e151831bba525f0889c12a3cb31bbab543b0148
Bea <ab291012@gmail.com>
2025-09-30 09:51:51 -0300

    feat: create complete movie routes

--- src/routes/movies.routes.js
+++ src/routes/movies.routes.js
@@ -1,5 +1,8 @@
 import express from "express";
-
+import MovieController from "../controllers/movies.controller.js";
 const movieRouter = express.Router();
+
+movieRouter.get("/api/movies", MovieController.findAll);
+movieRouter.post("/api/movies", MovieController.create);
 
 export default movieRouter;
